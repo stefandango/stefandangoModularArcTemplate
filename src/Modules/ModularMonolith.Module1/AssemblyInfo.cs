3abc8d8f4f8db70e631aabece0683b57c76f5ecf
Stefan <5796872+stefandango@users.noreply.github.com>
2024-03-10 12:08:20 +0100

    Added first test

---
+++ src/Modules/ModularMonolith.Module1/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("ModularMonolith.Module1.Tests")]
+namespace ModularMonolith.Module1;
+
+public class AssemblyInfo  {}
--- tests/ModularMonolith.Module1.Tests/Endpoints/Module1List.cs
+++ tests/ModularMonolith.Module1.Tests/Endpoints/Module1List.cs
@@ -1,11 +1,28 @@
+using FastEndpoints;
 using FastEndpoints.Testing;
+
+using FluentAssertions;
+
+using ModularMonolith.Module1.Endpoints;
 
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace ModularMonolith.Module1.Tests.Endpoints;
-/*
-public class Module1List (Fixture fixture, ITestOutputHelper outputHelper) : AppFixture<Fixture>(fixture, outputHelper)
-{ 
+public class Module1List (Fixture fixture, ITestOutputHelper outputHelper) : TestBase<Fixture>
+{
+    public ITestOutputHelper OutputHelper { get; } = outputHelper;
+
+    [Fact]
+    public async Task ReturnsThreeModule1ItemsAsync()
+    {
+        //Arrange
+        
+        // Act
+        var testResult = await fixture.Client.GETAsync<List, ListModule1Response>();
+        
+        //Assert
+        testResult.Response.EnsureSuccessStatusCode();
+        testResult.Result.Items.Count.Should().Be(3);
+    }
 }
-*/
